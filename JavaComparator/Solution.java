9a1624e39c3567db75ed6ce6d86e7ce6f13494d9
Dexter Richards <Dexter.Richards6@gmail.com>
2016-06-15 21:37:53 -0400

    Added Java Comparator

---
+++ JavaComparator/Solution.java
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/challenges/java-comparator/submissions/code/22153396
+
+class Checker implements Comparator<Player> {
+    public int compare(Player p1, Player p2) {
+        if (p1.score == p2.score) {
+            return p1.name.compareTo(p2.name);
+        }
+        else {
+            return p2.score - p1.score;
+        }
+    }
+}
